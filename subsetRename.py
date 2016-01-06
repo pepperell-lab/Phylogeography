5bf4e04f40a98a4b77377fa2b9005de5b57c71c1
ONeillMB1 <ONeillMB1@gmail.com>
2016-01-06 16:06:04 -0600

    edited for the tbarc meta file/fasta names

--- subsetRename.py
+++ subsetRename.py
@@ -3,6 +3,8 @@
 import sys
 from Bio import SeqIO
 from Bio.Alphabet import IUPAC,Gapped
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 import os
 import getopt
 
@@ -27,12 +29,14 @@ def rename_dict(metaFile):
 def rename_recordID(alnIN, d):
     """for every strain in the dictionary created above, \
     change the seq_record.id to reflect the info"""
-    out1 = alnIN.split(".")[0] + "_lin1.nexus"
-    out2 = alnIN.split(".")[0] + "_lin2.nexus"
-    out3 = alnIN.split(".")[0] + "_lin3.nexus"
+    #out1 = alnIN.split(".")[0] + "_lin1.nexus"
+    #out2 = alnIN.split(".")[0] + "_lin2.nexus"
+    #out3 = alnIN.split(".")[0] + "_lin3.nexus"
+    out4 = alnIN.split(".")[0] + "_lin4.nexus"
     lin1Records = []
     lin2Records = []
     lin3Records = []
+    lin4Records = []
     for seq_record in SeqIO.parse(alnIN, "fasta", alphabet=Gapped(IUPAC.ambiguous_dna, '-')):
         strain = seq_record.id.split("_")[0]
         print strain
@@ -42,9 +46,49 @@ def rename_recordID(alnIN, d):
             lin2Records.append(seq_record)
         elif strain in d[3]:
             lin3Records.append(seq_record)
-    SeqIO.write(lin1Records, out1, "nexus")
-    SeqIO.write(lin2Records, out2, "nexus")
-    SeqIO.write(lin3Records, out3, "nexus")
+        elif strain in d[4]:
+            lin4Records.append(seq_record)
+    #SeqIO.write(lin1Records, out1, "nexus")
+    #SeqIO.write(lin2Records, out2, "nexus")
+    #SeqIO.write(lin3Records, out3, "nexus")
+    SeqIO.write(lin4Records, out4, "nexus")
+
+#######
+def make_renameDict(metaFile):
+    """Create a dictionary that has the strain as the key \
+    and a list with 'iso2', 'who_region', 'lat', and 'long' \
+    as the value"""
+    d = {}
+    with open(metaFile, 'r') as meta:
+        for line in meta:
+            line = line.strip().split('\t')
+            srs = line[0]
+            samp = line[2].lower().replace("-","_")
+            iso2 = line[7]
+            who = line[8]
+            #lat = line[8]
+            #lon = line[9]
+            d[samp] = [srs, iso2, who] #, lat, lon]
+    return d
+
+def rename_Aln(alnIN, d):
+    """for every strain in the dictionary, change the \
+    seq_record.id to reflect the info"""
+    alnOUT = alnIN.split(".")[0] + "_recoded.fasta"
+    records = []
+    for seq_record in SeqIO.parse(alnIN, "fasta", 
+    alphabet=Gapped(IUPAC.ambiguous_dna, "-")):
+        strain = "_".join(seq_record.id.split("_")[2:])
+        print strain
+        if strain in d:
+            newID = "_".join(d[strain])
+            newSeq = str(seq_record.seq).replace("N", "-")
+            newRecord = SeqRecord(Seq(newSeq, Gapped(IUPAC.ambiguous_dna, "-")),
+            id = newID, description = "Mtb")
+            records.append(newRecord)
+        else:
+            print("problem with {0}".format(strain))
+    SeqIO.write(records, alnOUT, "fasta")
 
 # check for correct arguments
 if len(sys.argv) < 3:
@@ -53,5 +97,7 @@ if len(sys.argv) < 3:
 
 script, alnIN, metaFile = sys.argv
 
-d = rename_dict(metaFile)
-rename_recordID(alnIN, d)
+#d = rename_dict(metaFile)
+#rename_recordID(alnIN, d)
+d = make_renameDict(metaFile)
+rename_Aln(alnIN, d)
